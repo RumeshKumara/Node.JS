043b04538dcb13c2ef660ec496a120826d6d529f
rumesh <rumeshk066@gmail.com>
2025-02-05 11:20:45 +0530

    call back api

--- node-tutorial-3/index.js
+++ node-tutorial-3/index.js
@@ -1,5 +1,9 @@
 import {fileURLToPath} from 'node:url';
-import {dirname} from 'node:path';
+import {dirname, join} from 'node:path';
+import { readFile } from 'node:fs';
+import { writeFile } from 'node:fs/promises';
+import { appendFile } from 'node:fs/promises';
+import { rm } from 'node:fs/promises';
 
 // If createRequire is not used, remove the import statement
 // Otherwise, ensure it is used correctly in the code
@@ -14,6 +18,55 @@ import {dirname} from 'node:path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-console.log('__filename', __filename);
-console.log('__dirname', __dirname);
+// Example usage of readFile
+ // Replace 'read.txt' with the actual file name and path.
+ // This example reads a text file and logs its contents to the console.
+ // Adjust the encoding to 'utf-8' if your file contains non-ASCII characters.
+ // If you want to handle errors, add an error handling callback to the readFile function.
+ // For example:
+ // readFile(join(__dirname,'read.txt'), {encoding: 'utf-8'}, (err, data) => {
+readFile(join(__dirname, 'read.txt'), {
+    encoding: 'utf-8',
+  
+},
 
+(err, data) => {
+    if (!err){
+    console.log(data);
+    }
+});
+
+// Example usage of writeFile
+// Replace 'write.txt' with the actual file name and path.
+// This example writes a string to a text file.
+writeFile(join(__dirname, 'write.txt'), 'Hello from Node.js', (err) => {
+    if (!err){
+    console.log('File created or overwritten');
+    }
+});
+
+// Example usage of appendFile
+// Replace 'write.txt' with the actual file name and path.
+
+// This example appends a string to a text file.
+appendFile(join(__dirname, 'write.txt'), ' Hello from Node.js', (err) => {
+    if (!err){
+    console.log('Content appended to file');
+    }
+});
+
+// Example usage of appendFile
+appendFile(join(__dirname, 'write.js'),
+ `const a = 21;
+ const b = 21;`, 
+ (err) => {
+    if (!err){
+    console.log('Content appended to file');
+    }
+});
+
+rm(join(__dirname, 'write.js'), (err) => {
+    if (!err){
+    console.log('File deleted');
+    }
+});
